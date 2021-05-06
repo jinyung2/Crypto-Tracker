e1971b4ff821cf7934ebb656f23ac2b7e579c1bc
jinyung2 <jinyoungchoi118@gmail.com>
2021-05-05 17:15:43 -0700

    added prettier into dependency list, added some options into package.json for auto styling on commit

--- frontend/src/Pages/SignUpLogIn/SignUpLogIn.js
+++ frontend/src/Pages/SignUpLogIn/SignUpLogIn.js
@@ -2,110 +2,135 @@ import "./SignUpLogIn.css";
 import { Container, Row, Col, Alert } from "react-bootstrap";
 import { Route } from "react-router-dom";
 import { useState } from "react";
-import logo from '../../assets/square.jpg';
-import Signup from '../../Components/SignUpForm/SignUp';
-import LogIn from '../../Components/LoginForm/LogInForm';
+import logo from "../../assets/square.jpg";
+import Signup from "../../Components/SignUpForm/SignUp";
+import LogIn from "../../Components/LoginForm/LogInForm";
 
 function LoginSignUp() {
-    const [up, setUP] = useState({
-        email: "",
-        password: "",
-        reEnterPass: ""
-    })
-    const [loginCred, setLoginCred] = useState({
-        email: "",
-        password: ""
-    })
+  const [up, setUP] = useState({
+    email: "",
+    password: "",
+    reEnterPass: "",
+  });
+  const [loginCred, setLoginCred] = useState({
+    email: "",
+    password: "",
+  });
 
-    const [error, setError] = useState("");
+  const [error, setError] = useState("");
 
-    //assigns username and password values to "up" variable
-    function handleSignUpChange(event) {
-        const { name, value } = event.target;
-        if (name === "password") {
-            setUP(
-                { email: up["email"], password: value, reEnterPass: up["reEnterPass"] }
-            )
-        }
-        else if (name === "repassword") {
-            setUP(
-                { email: up["email"], password: up["password"], reEnterPass: value }
-            )
-        }
-        else {
-            setUP(
-                { email: value, password: up["password"], reEnterPass: up["reEnterPass"] }
-            )
-        }
+  //assigns username and password values to "up" variable
+  function handleSignUpChange(event) {
+    const { name, value } = event.target;
+    if (name === "password") {
+      setUP({
+        email: up["email"],
+        password: value,
+        reEnterPass: up["reEnterPass"],
+      });
+    } else if (name === "repassword") {
+      setUP({
+        email: up["email"],
+        password: up["password"],
+        reEnterPass: value,
+      });
+    } else {
+      setUP({
+        email: value,
+        password: up["password"],
+        reEnterPass: up["reEnterPass"],
+      });
     }
+  }
 
-    //check if inputs are valid
-    function submitSignUpForm() {
-        //if not all criteria is filled out, then alert
-        if (!validateEmail(up.email) || up.password === "" || up.reEnterPass === "") {
-            setError("Sign up credentials are invalid. Please try again.")
-        }
-        //else if passwords don't match, then alert 
-        else if (up.password !== up.reEnterPass) {
-            setError("Passwords do not match. Please re-enter passwords.");
-        } else {
-            clearError();
-        }
-        //else then no errors and add to database and redirect user to dashbaord
+  //check if inputs are valid
+  function submitSignUpForm() {
+    //if not all criteria is filled out, then alert
+    if (
+      !validateEmail(up.email) ||
+      up.password === "" ||
+      up.reEnterPass === ""
+    ) {
+      setError("Sign up credentials are invalid. Please try again.");
     }
-
-    function handleLogInChange(event) {
-        const { name, value } = event.target;
-        if (name === "email") {
-            setLoginCred({
-                email: value,
-                password: loginCred.password
-            })
-        } else if (name === "password") {
-            setLoginCred({
-                email: loginCred.email,
-                password: value
-            })
-        }
+    //else if passwords don't match, then alert
+    else if (up.password !== up.reEnterPass) {
+      setError("Passwords do not match. Please re-enter passwords.");
+    } else {
+      clearError();
     }
+    //else then no errors and add to database and redirect user to dashbaord
+  }
 
-    function handleLogInForm() {
-        console.log(loginCred.password.length);
-        // Set password validation to be >= 6 characters
-        if (!validateEmail(loginCred.email) || loginCred.password.length < 6) {
-            setError("Log In Credentials are invalid. Please try again.");
-        } else {
-            clearError();
-        }
+  function handleLogInChange(event) {
+    const { name, value } = event.target;
+    if (name === "email") {
+      setLoginCred({
+        email: value,
+        password: loginCred.password,
+      });
+    } else if (name === "password") {
+      setLoginCred({
+        email: loginCred.email,
+        password: value,
+      });
     }
+  }
 
-    function validateEmail(email) {
-        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(email).toLowerCase());
+  function handleLogInForm() {
+    console.log(loginCred.password.length);
+    // Set password validation to be >= 6 characters
+    if (!validateEmail(loginCred.email) || loginCred.password.length < 6) {
+      setError("Log In Credentials are invalid. Please try again.");
+    } else {
+      clearError();
     }
+  }
 
-    function clearError() { setError(""); }
+  function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
 
-    return (
-        <div id="signUpLogin">
-            <Container fluid>
-                <Row style={{ textAlign: "center" }} className="align-items-center justify-content-center">
-                    <Col id="vr">
-                        <img id="logo" src={logo} alt="logo" />
-                    </Col>
-                    <Col className="justify-content-center">
-                        <Route path="/signup">
-                            <Signup handleChange={handleSignUpChange} submitForm={submitSignUpForm} clearError={clearError}></Signup>
-                        </Route>
-                        <Route path="/login">
-                            <LogIn handleChange={handleLogInChange} submitForm={handleLogInForm} clearError={clearError}></LogIn>
-                        </Route>
-                        {error ? <Alert style={{ margin: "5px 20%" }} variant="danger">{error}</Alert> : null}
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    )
+  function clearError() {
+    setError("");
+  }
+
+  return (
+    <div id="signUpLogin">
+      <Container fluid>
+        <Row
+          style={{ textAlign: "center" }}
+          className="align-items-center justify-content-center"
+        >
+          <Col id="vr">
+            <img id="logo" src={logo} alt="logo" />
+          </Col>
+          <Col className="justify-content-center">
+            <Route path="/signup">
+              <Signup
+                handleChange={handleSignUpChange}
+                submitForm={submitSignUpForm}
+                clearError={clearError}
+              ></Signup>
+            </Route>
+            <Route path="/login">
+              <LogIn
+                handleChange={handleLogInChange}
+                submitForm={handleLogInForm}
+                clearError={clearError}
+              ></LogIn>
+            </Route>
+            {error ? (
+              <Alert style={{ margin: "5px 20%" }} variant="danger">
+                {error}
+              </Alert>
+            ) : null}
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default LoginSignUp;

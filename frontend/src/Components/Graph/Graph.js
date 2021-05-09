8854ceb6979df8f6841d033148da3f3d957d364f
jinyung2 <jinyoungchoi118@gmail.com>
2021-05-09 03:01:39 -0700

    experiemented with more inline styles and options for various graphs.

--- frontend/src/Components/Graph/Graph.js
+++ frontend/src/Components/Graph/Graph.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Bar, Doughnut } from 'react-chartjs-2';
+import { Bar, Doughnut, Radar } from 'react-chartjs-2';
 import './Graph.css';
 
 const rand = () => Math.round(Math.random() * 255);
@@ -11,7 +11,7 @@ const data = {
       type: 'line',
       label: 'Dataset 1',
       borderColor: 'rgb(54, 162, 235)',
-      backgroundColor: 'black',
+      backgroundColor: 'white',
       borderWidth: 2,
       fill: false,
       data: [rand(), rand(), rand(), rand(), rand(), rand(), rand()],
@@ -21,7 +21,7 @@ const data = {
       label: 'Dataset 2',
       backgroundColor: `rgb(${rand()}, ${rand()}, ${rand()})`,
       data: [rand(), rand(), rand(), rand(), rand(), rand(), rand()],
-    //   borderColor: 'white',
+        borderColor: 'white',
       borderWidth: 2,
     },
     {
@@ -34,34 +34,34 @@ const data = {
 };
 
 const data2 = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
-  
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
 
 const options = {
+  maintainAspectRatio: false,
   plugins: {
     legend: {
       display: false,
@@ -95,20 +95,47 @@ const options = {
   },
 };
 
+const types = ['bar', 'doughnut', 'radar'];
 
 function Graph() {
-    const [type, setType] = useState('bar');
-    function toggleType() {
-        const newType = type === 'bar' ? 'doughnut' : 'bar';
-        console.log(newType);
-        setType(newType);
-    }
+  const [type, setType] = useState('bar');
+  function toggleType() {
+    const index = (types.indexOf(type) + 1) % types.length;
+    setType(types[index]);
+  }
   return (
     <div class="root">
       <h1 className="title">MultiType Chart</h1>
-      {type === 'doughnut' ? <div style={{display: 'flex', justifyContent: 'center'}}><Doughnut data={data2} height={500} options={{maintainAspectRatio: false}} /></div> : null}
-      {type === 'bar' ? <Bar data={data} height={2} width={4} options={options} /> : null}
-      <div onClick={toggleType}><h1 style={{border: '1px solid white'}}>Click here to Toggle Chart</h1></div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        {type === 'doughnut' ? (
+          <Doughnut
+            data={data2}
+            height={500}
+            options={{ maintainAspectRatio: false, plugins: {legend: {display: false}}}}
+          />
+        ) : null}
+        {type === 'radar' ? (
+          <Radar
+            data={data2}
+            height={500}
+            options={{ maintainAspectRatio: false, plugins: {legend: {display: false}}, scales: {
+                r: {
+                    grid: {
+                        color: ['white', 'purple']
+                    }
+                }
+            }}}
+          ></Radar>
+        ) : null}
+        {type === 'bar' ? (
+          <Bar data={data} height={500} width={1000} options={options} />
+        ) : null}
+      </div>
+      <div onClick={toggleType}>
+        <h1 style={{ border: '1px solid white' }}>
+          Click here to Toggle Chart
+        </h1>
+      </div>
     </div>
   );
 }

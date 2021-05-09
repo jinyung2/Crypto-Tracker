ee45aa74633a351d1a29d28760ae1966a7b5ab41
jinyung2 <jinyoungchoi118@gmail.com>
2021-05-08 20:45:51 -0700

    tried experimenting with example multi type graphs

--- frontend/src/Components/Graph/Graph.js
+++ frontend/src/Components/Graph/Graph.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import './Graph.css';
+
+const rand = () => Math.round(Math.random() * 20 - 10);
+
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      type: 'line',
+      label: 'Dataset 1',
+      borderColor: 'rgb(54, 162, 235)',
+      borderWidth: 2,
+      fill: false,
+      data: [rand(), rand(), rand(), rand(), rand(), rand()],
+    },
+    {
+      type: 'bar',
+      label: 'Dataset 2',
+      backgroundColor: 'rgb(255, 99, 132)',
+      data: [rand(), rand(), rand(), rand(), rand(), rand(), rand()],
+      borderColor: 'white',
+      borderWidth: 2,
+    },
+    {
+      type: 'bar',
+      label: 'Dataset 3',
+      backgroundColor: 'rgb(75, 192, 192)',
+      data: [rand(), rand(), rand(), rand(), rand(), rand(), rand()],
+    },
+  ],
+};
 
 function Graph() {
-    return (
-        <h1 style={{backgroundColor: "white"}}> Graph goes here</h1>
-    )
+  return (
+    <div class='container'>
+      <div className="header">
+        <h1 className="title">MultiType Chart</h1>
+      </div>
+      <Bar data={data} height={200} width={200} />
+    </div>
+  );
 }
 
 export default Graph;

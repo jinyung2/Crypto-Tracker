654a6a338b3ca29aa2275b83eb6bea9773e94a13
jinyung2 <jinyoungchoi118@gmail.com>
2021-05-19 07:54:00 -0700

    added coin name and price above graph, implemented some more graph detail, fixed graph display related bugs

--- frontend/src/Components/Graph/Graph.js
+++ frontend/src/Components/Graph/Graph.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Bar, Doughnut, Radar, Line } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 import axios from 'axios';
+import { ScaleLoader } from 'react-spinners';
 import './Graph.css';
-
-const rand = () => Math.round(Math.random() * 255);
 
 let options = {
     maintainAspectRatio: false,
@@ -19,7 +18,7 @@ let options = {
         },
         ticks: {
           font: {
-            size: 10,
+            size: 15,
             weight: 'bold',
           },
         },
@@ -32,43 +31,41 @@ let options = {
         ticks: {
           color: 'pink',
           font: {
-            size: 30,
-            weight: 200,
+            size: 10,
           },
         },
       },
     },
-    responsive: true
   };
 
-const types = ['bar', 'doughnut', 'radar'];
 function Graph(props) {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState({});
-  const diff = 10;
+  const diff = 50;
 useEffect(() => {
-    axios.get('http://localhost:5000/coin/bitcoin/h12').then(async (res) => {
-        console.log(res.data);
-        const labels = await res.data.data
-        .map(() => '')
-        .filter((val, i, arr) => i > 0 && i % diff === 0 || (i == arr.length - 1));
+    setLoading(true);
+    axios.get(`http://localhost:5000/coin/${props.coin}/m1`).then((res) => {
+        const labels = res.data.data
+        .map((data, i) => '')
+        .filter((val, i, arr) => (i > 0 && ((i % diff) === 0)) || (i === arr.length - 1));
 
-        const data = await res.data.data
+        const graphData = res.data.data
         .map((data, i, arr) =>
         i >= diff
         ? [+arr[i - diff].priceUsd, +arr[i].priceUsd]
         : [0, +data.priceUsd]
         )
-        .filter((val, i, arr) => i > 0 && i % diff === 0 || (i == arr.length - 1));
+        .filter((val, i, arr) => (i > 0 && i % diff === 0) || (i === arr.length - 1));
+
+        console.log(graphData);
 
         let borderColor = ['rgba(0, 177, 106, 1)'];
-        for (let i = 1; i < data.length; i++) {
-            borderColor.push(data[i][0] > data[i][1] ? 'red' : 'rgba(0, 177, 106, 1)');
+        for (let i = 1; i < graphData.length; i++) {
+            borderColor.push(graphData[i][0] > graphData[i][1] ? 'red' : 'rgba(0, 177, 106, 1)');
         }
-        console.log(data);
-        const min = Math.floor(data.flatMap(e => e).reduce((a,b) => Math.min(a,b)));
-        const max = Math.ceil(data.flatMap(e => e).reduce((a,b) => Math.max(a,b)));
-        options = {...options, scales: {...options.scales, yAxes: {...options.scales.yAxes, min: min, max: max}}};
+        const min = graphData.flatMap(e => e).reduce((a,b) => Math.min(a,b));
+        const max = graphData.flatMap(e => e).reduce((a,b) => Math.max(a,b));
+        options = {...options, scales: {...options.scales, yAxes: {...options.scales.yAxes, min: min * 0.95, max: max * 1.05}}};
       setData({
         labels: labels,
         datasets: [
@@ -85,7 +82,7 @@ useEffect(() => {
             categoryPercentage: 1.0,
             barPercentage: 0.7,
             label: '',
-            data: data,
+            data: graphData,
             backgroundColor: borderColor,
             borderWidth: 1,
           },
@@ -93,12 +90,11 @@ useEffect(() => {
       });
       setLoading(false);
     });
-  }, []);
+  }, [setData]);
 
   return (
     <div className="root">
-      <h1 className="title">MultiType Chart</h1>
-      {loading ? <div style={{width: '100%', height: '500px'}}>LOADING</div> : <div style={{ display: 'flex', justifyContent: 'center' }}>
+      {loading ? <div className='loading'><h1>Loading...</h1><ScaleLoader width={7} color='#fff'/></div> : <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Bar data={data} height={500} options={options} />
       </div>}
       <div>
--- frontend/src/Pages/Dashboard/Dashboard.js
+++ frontend/src/Pages/Dashboard/Dashboard.js
@@ -3,14 +3,24 @@ import { Container, Row, Col } from 'react-bootstrap';
 import Graph from '../../Components/Graph/Graph';
 import './Dashboard.css';
 import DashboardNavBar from '../../Components/DashboardNavBar/DashboardNavBar';
+import { BarLoader } from 'react-spinners';
 import axios from 'axios';
 
-
+function getCoinInfo(name, setFn) {
+  axios.get(`http://localhost:5000/coin/${name}`).then((res) => {
+    setFn(res.data);
+  });
+}
 
 function Dashboard() {
-    const [coinData, setCoinData] = useState();
-    const [graphData, setGraphData] = useState();
-    const [watchlist, setWatchList] = useState();
+    const [coin, setCoin] = useState('dogecoin');
+  // coinData for the current price, name display
+  const [coinData, setCoinData] = useState(null);
+  const [watchlist, setWatchList] = useState(null);
+
+  useEffect(() => {
+    getCoinInfo(coin, setCoinData);
+  }, []);
 
   return (
     <Fragment>
@@ -19,7 +29,15 @@ function Dashboard() {
         <Row>
           <Col lg={8}>
             <Row>
-              <Graph />
+              {coinData ? 
+                <div className="coin-container">
+                  <div className="coin-title">
+                    {coinData.name}: {coinData.symbol}
+                  </div>
+                  <div className="coin-price">${+coinData.priceUsd}</div>
+                </div> : <div className="coin-container"><BarLoader width={300} color='#fff'/></div>
+              }
+              <Graph coin={coin} />
             </Row>
             <Row>
               <h1>Info here</h1>

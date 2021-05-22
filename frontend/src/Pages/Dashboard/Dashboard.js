8522a5a07ffe28b00676c5eceb1a633f630e2a55
Wesly Ortega <weslyortega@gmail.com>
2021-05-22 15:33:57 -0700

    merged with master

--- frontend/src/Pages/Dashboard/Dashboard.js
+++ frontend/src/Pages/Dashboard/Dashboard.js
@@ -1,13 +1,26 @@
-import { React, Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Graph from '../../Components/Graph/Graph';
 import './Dashboard.css';
 import DashboardNavBar from '../../Components/DashboardNavBar/DashboardNavBar';
-import Info from '../../Components/Info/Info'
+import { BarLoader } from 'react-spinners';
+import axios from 'axios';
+
+function getCoinInfo(name, setFn) {
+  axios.get(`http://localhost:5000/coin/${name}`).then((res) => {
+    setFn(res.data);
+  });
+}
 
 function Dashboard() {
-
   const [coin, setCoin] = useState('dogecoin');
+  // coinData for the current price, name display
+  const [coinData, setCoinData] = useState(null);
+  const [watchlist, setWatchList] = useState(null);
+
+  useEffect(() => {
+    getCoinInfo(coin, setCoinData);
+  }, [setCoin]);
 
   return (
     <Fragment>
@@ -16,7 +29,19 @@ function Dashboard() {
         <Row>
           <Col lg={8}>
             <Row>
-              <Graph />
+              {coinData ? (
+                <div className="coin-container">
+                  <div className="coin-title">
+                    {coinData.name}: {coinData.symbol}
+                  </div>
+                  <div className="coin-price">${+coinData.priceUsd}</div>
+                </div>
+              ) : (
+                <div className="coin-container">
+                  <BarLoader width={300} color="#fff" />
+                </div>
+              )}
+              <Graph coin={coin} />
             </Row>
             <Row>
               <Info coin={coin}/>

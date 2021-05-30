f843a43c938f5c407a1d6f364c191c7c9e816189
jinyung2 <jinyoungchoi118@gmail.com>
2021-05-30 02:47:39 -0700

    reformatted some jsx code, navbar moved to SignupLogin component, and redid responsive styling

--- frontend/src/Components/SignUpForm/SignUp.js
+++ frontend/src/Components/SignUpForm/SignUp.js
@@ -1,59 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./../../Pages/SignUpLogIn/SignUpLogIn.css";
+import React from 'react';
+import './../../Pages/SignUpLogIn/SignUpLogIn.css';
 
 function Signup(props) {
   return (
-    <div>
-      <div className="nav-buttons">
-        <Link id="loginLink" to="/auth/login">
-          <button className="lsButton" onClick={props.clearError}>
-            Login
-          </button>
-        </Link>
-        <Link id="signUpBtn" to="/auth/signup">
-          <button
-            style={{ backgroundColor: "#979797" }}
-            className="lsButton"
-            onClick={props.clearError}
-          >
-            Sign Up
-          </button>
-        </Link>
-      </div>
-      <form id="signUpForm">
-        <input
-          className="form-control form-control-lg signUpInput"
-          type="text"
-          name="email"
-          placeholder="Email"
-          onChange={props.handleChange}
-        />
-        <br />
-        <input
-          className="form-control form-control-lg signUpInput"
-          type="password"
-          name="password"
-          onChange={props.handleChange}
-          placeholder="Password"
-        />
-        <br />
-        <input
-          className="form-control form-control-lg signUpInput"
-          type="password"
-          name="repassword"
-          onChange={props.handleChange}
-          placeholder="Re-enter Password"
-        />
-        <br />
-        <input
-          className="btn signUpSubmit"
-          type="button"
-          value="Sign Up"
-          onClick={props.submitForm}
-        />
-      </form>
-    </div>
+    <form id="signUpForm">
+      <input
+        className="form-control signUpInput"
+        type="text"
+        name="email"
+        placeholder="Email"
+        onChange={props.handleChange}
+      />
+      <br />
+      <input
+        className="form-control signUpInput"
+        type="password"
+        name="password"
+        onChange={props.handleChange}
+        placeholder="Password"
+      />
+      <br />
+      <input
+        className="form-control signUpInput"
+        type="password"
+        name="repassword"
+        onChange={props.handleChange}
+        placeholder="Re-enter Password"
+      />
+      <br />
+      <input
+        className="btn signUpSubmit"
+        type="button"
+        value="Sign Up"
+        onClick={props.submitForm}
+      />
+    </form>
   );
 }
 

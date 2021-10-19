9d3c95317d0ae02f70e6595f188e175bde67b637
jinyung2 <jinyoungchoi118@gmail.com>
2021-10-18 20:18:34 -0700

    accidentally commented out getWatchlist

--- frontend/src/Api/User.js
+++ frontend/src/Api/User.js
@@ -3,7 +3,7 @@ import { userApi } from './';
 export const user = {
     getCoin: (coin) => userApi.get(`/coin/${coin}`),
     getCoinInter: (coin, inter) => userApi.get(`/coin/${coin}/${inter}`),
-    // getWatchlist: () => userApi.get(`/watchlist`),
+    getWatchlist: () => userApi.get(`/watchlist`),
     addToWatchlist: (coinData) => userApi.post(`/watchlist/${coinData}`),
     updateWatchlist: (newWatchlist) => userApi.put(`/watchlist`, {watchlist: newWatchlist}),
     removeFromWatchlist: (coin) => userApi.delete(`/watchlist/${coin}`)
